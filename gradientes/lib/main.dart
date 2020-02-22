7c5301c1e122a94cf6454336bf383690a646f2c1
Freddie <freddy.abadl@ucuenca.edu.ec>
2020-02-22 14:46:42 -0500

    Gradiente y Bordes Redondeados

--- gradientes/lib/main.dart
+++ gradientes/lib/main.dart
@@ -8,26 +8,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Quitar Banner inicial
       debugShowCheckedModeBanner: false,
       title: 'Gradientes',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Container(
-        decoration: new BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.yellow, Colors.orange, Colors.red],
-          stops: [0.3, 0.5, 0.8],
-          begin: FractionalOffset.topRight,
-          end: FractionalOffset.bottomLeft
-          )
-        ),
-        child: Center(
-          child: RaisedButton(
-            child: Text("Aceptar"),
-            onPressed: (){}
-          )
-        )
-        ,
+        //margen
+        color: Colors.white,
+        child: Container(
+            margin: EdgeInsets.all(20),
+            decoration: new BoxDecoration(
+                //margin con bordes redondeados
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20)),
+                gradient: LinearGradient(
+                    colors: [Colors.yellow, Colors.orange, Colors.red],
+                    stops: [0.3, 0.5, 0.8],
+                    begin: FractionalOffset.topRight,
+                    end: FractionalOffset.bottomLeft)),
+            child: Center(
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20))),
+                    child: Text("Aceptar"),
+                    onPressed: () {}))),
       ),
     );
   }
